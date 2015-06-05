36120e73609b1a44f08028674a9ae1bba7af6772
local_j <jb@isiteletrade.com>
2015-06-05 16:37:36 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ScrollEffect Test</title>
+    <link rel="stylesheet" href="css/scrollEffect.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/scrollEffect.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+    
+    <div class="item">
+        <div data-animate-entry="rotateRight">
+            <p >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+            </p>
+        </div>                
+    </div>
+    <table>
+        <tr>
+            <td class="item" data-animate-entry="easeRight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae accusamus exercitationem facilis maxime, repudiandae animi magnam obcaecati beatae laboriosam deleniti consequuntur in numquam tempora sunt, praesentium error ipsa repellat voluptatem.</td>
+            <td class="item" data-animate-entry="ascend">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione sed obcaecati unde odio quisquam nam ullam quasi? Placeat et voluptas iste explicabo quod error, ad in, consequuntur, commodi vero beatae!</td>
+            <td class="item" data-animate-entry="easeLeft">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti doloribus ut, dolore earum at, sed culpa ducimus magnam vel, quis temporibus iste, ad quisquam? Commodi at ipsam quisquam, reprehenderit illo!</td>
+        </tr>
+    </table>
+     <div class="item" data-animate-entry="ascend" animate-entry-speed="3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+     <div class="item" data-animate-entry="descend">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+     <div class="item" data-animate-entry="easeRight">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+    <div class="item" data-animate-entry="easeLeft">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+     <div class="item" data-animate-entry="rotateRight">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+     <div class="item">
+        <p style="padding-top:150px" data-animate-entry="rotateRight easeLeft">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.    
+        </p>        
+    </div>
+     <div class="item" data-animate-entry>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+    <div class="item" data-animate-entry>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+     <div class="item" data-animate-entry>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+     <div class="item" data-animate-entry>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>
+    <div class="item" data-animate-entry>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas dolorem voluptatibus et dolore cumque rem alias nulla. Velit tenetur nam nostrum aperiam laboriosam cum officia commodi sunt adipisci asperiores.
+    </div>    
+</div>
+</body>
+</html>
---
+++ js/scrollEffect.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+
+  ;(function () {
+
+    var w = $(window);
+
+    // default options
+    var defaults = {
+      animation : '',
+      speed : 0.5,
+      delay: 0
+    }
+    // available animations
+    var validAnims = [
+      'descend',
+      'ascend',
+      'easeRight',
+      'easeLeft'
+    ]
+
+
+    // ScrollEffectObject Class
+    function ScrollEffectObject (elem,o) {
+      
+      // setup options
+      var o = typeof o === 'object' ? o : {},
+          opts = $.extend({},defaults,o);
+
+      // setup vars
+      var elem = elem,
+          wHeight = w.height(),
+          elemTop = elem.offset().top,
+          breakpoint = elemTop - (wHeight*0.4);
+
+      elem.css({
+        '-webkit-transition': 'all '+ opts.speed +'s '+ opts.delay +'s ease',
+        '-o-transition': 'all '+ opts.speed +'s '+ opts.delay +'s ease',
+        'transition': 'all '+ opts.speed +'s '+ opts.delay +'s ease'
+      });
+
+      $(window).scroll(function(){     
+
+          var scrollTop = w.scrollTop();
+          
+          if ( scrollTop > breakpoint && !elem.hasClass('animated')) 
+          {
+              elem.addClass('animated');
+          }
+      
+      }).resize(function(){        
+          if ( elem.offset().top !=  elemTop )    {
+              elemTop = elem.offset().top;
+              breakpoint = elemTop - (wHeight/2);
+          }
+      });
+
+    }
+
+    // instantiate ScrollEffectObject
+    $('[data-animate-entry]').each(function(){
+       
+      var e = $(this),
+          animation = e.attr('data-animate-entry'),
+          speed = e.attr('animate-entry-speed'),
+          delay = e.attr('animate-entry-delay') | 0,
+          obj = new ScrollEffectObject(e,{animation,speed,delay});      
+
+    });
+
+  })();
+  
+});
